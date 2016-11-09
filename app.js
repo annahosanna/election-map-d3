efa93d7a915d786bdebe8ba5bea89f89b9b8efed
Valerie Gibson <vgibson14@gmail.com>
2016-11-09 20:46:17 +0000

    converts to functions

--- app.js
+++ app.js
@@ -3,11 +3,11 @@ window.onload = function() {
   var width = 1000;
   var height = 500;
 
-  var projection = map.createProjection(width, height);
-  var path = map.createPath(projection);
+  var projection = createProjection(width, height);
+  var path = createPath(projection);
 
-  var svg = map.createSVG(width, height);
+  var svg = createSVG(width, height);
 
-  map.addData(svg, path);
+  addData(svg, path);
 
 }
--- map.js
+++ map.js
@@ -1,24 +1,25 @@
-var map = {
-  createPath: (projection) => {
-    return d3.geo.path().projection(projection);
-  },
-  createProjection: (width, height) => {
-    return d3.geo.albersUsa().translate([width/2, height/2]).scale([900]);
-  },
-  createSVG: (width, height) => {
-    return d3.select("body")
-      .append("svg")
-      .attr("width", width)
-      .attr("height", height);
-  },
-  addData: (svg, path) => {
-    d3.json("us-states.json", function(json) {
-      svg.selectAll("path")
-      .data(json.features)
-      .enter()
-      .append("path")
-      .attr("d", path)
-      .style("fill", "steelblue");
-    });
-  }
+const createPath = (projection) => {
+  return d3.geo.path().projection(projection);
+}
+
+const createProjection = (width, height) => {
+  return d3.geo.albersUsa().translate([width/2, height/2]).scale([900]);
+}
+
+const createSVG = (width, height) => {
+  return d3.select("body")
+  .append("svg")
+  .attr("width", width)
+  .attr("height", height);
+}
+
+const addData = (svg, path) => {
+  d3.json("us-states.json", function(json) {
+    svg.selectAll("path")
+    .data(json.features)
+    .enter()
+    .append("path")
+    .attr("d", path)
+    .style("fill", "steelblue");
+  });
 }
