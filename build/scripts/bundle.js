a6c6d1d9464e973916874a2ed510b072132f1ac3
Valerie Gibson <vgibson14@gmail.com>
2016-11-10 11:55:41 +0000

    last tidy

--- build/scripts/bundle.js
+++ build/scripts/bundle.js
@@ -54,10 +54,11 @@
 	window.onload = function() {
 	
 	  const statsSearch = new StatsSearch(election_data)
-	  const infoView = new InfoView()
 	
+	  const infoView = new InfoView()
 	  const observers = [infoView]
 	  const classes = new MapClassHandler
+	  
 	  const mapEvents = new MapEvents({observers, classes, statsSearch})
 	  const map = new Map({width: 800, height: 500, scale: 1000, events: mapEvents})
 	
