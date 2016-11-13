d7994a131494cd167db4546bea9085d163fd81d6
Valerie Gibson <vgibson14@gmail.com>
2016-11-13 20:49:24 +0000

    highlights

--- src/app.js
+++ src/app.js
@@ -8,10 +8,11 @@ const election_data = require('./data/election-data')
 window.onload = function() {
 
   const statsSearch = new StatsSearch(election_data)
+  const classes = new MapClassHandler
 
   const infoView = new InfoView()
   const observers = [infoView]
-  const classes = new MapClassHandler
+
   
   const mapEvents = new MapEvents({observers, classes, statsSearch})
   const map = new Map({width: 800, height: 500, scale: 1000, events: mapEvents, classes: classes, statsSearch: statsSearch})
--- src/info_view.js
+++ src/info_view.js
@@ -1,13 +1,21 @@
 const Handlebars = require('handlebars')
 
 class InfoView {
+
+  constructor() {
+    Handlebars.registerHelper('voteCheck', function (party1, party2, className) {
+        return party1 > party2 ? className : '';
+    });
+  }
+
   notify(matched) {
     const element = document.querySelector("#info div")
     element.style.visibility = "visible"
     const source   = document.querySelector("#state-info").innerHTML
     const template = Handlebars.compile(source)
     
-    const html = template(this.prettify(matched))
+    const context = this.prettify(matched);
+    const html = template(context)
     element.innerHTML = html
   }
 
